2becb3786d55e9197132877ef15122971c010425
Solymnos <113204065+Solymnos@users.noreply.github.com>
2025-05-11 23:11:26 +0200

    update projects with real project, need to be update regulary

--- app/src/components/ProjectItem.jsx
+++ app/src/components/ProjectItem.jsx
@@ -1,15 +1,26 @@
+import { useEffect , useState } from "react"
+
 let ProjectItem = ({item, selectedId, selectProject}) =>
 {
+    const [index, setIndex] = useState(0);
+    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prev) => (prev + 1) % item.images.length);
+        }, 8000);
+        return () => clearInterval(interval);
+    }, [item.images.length])
+
     if (selectedId == -1)
     {
         return (
             <div className="p-4 lg:p-8 w-full lg:w-1/2 h-full">
-                <div onClick={() => selectProject(item.id)} className="hover:border-[#FF5E8A] cursor-pointer p-8 flex flex-row  justify-between border-2 border-[#B051EE] w-full min-h-3 rounded-4xl">
+                <div onClick={() => selectProject(item.id)} className="min-h-60 items-center h-full hover:border-[#FF5E8A] cursor-pointer p-8 flex flex-row  justify-between border-2 border-[#B051EE] w-full rounded-4xl gap-8">
                     <div className="flex flex-col">
                         <h1 className="montserrat font-bold text-white text-4xl">{item.name}</h1>
                         <p className="montserrat text-white">{item.description}</p>
                     </div>
-                    <div>
+                    <div className="flex h-full">
                         <img className="max-w-24 max-h-24" src={item.icon}/>
                     </div>
                 </div>
@@ -20,16 +31,20 @@ let ProjectItem = ({item, selectedId, selectProject}) =>
     {
         return (
             <div className="p-8 w-full h-full">
-                <div onClick={() => selectProject(-1)} className="hover:border-[#FF5E8A] cursor-pointer p-8 flex flex-row  justify-between border-2 border-[#B051EE] w-full min-h-3 rounded-4xl">
+                <div onClick={() => selectProject(-1)} className="hover:border-[#FF5E8A] cursor-pointer p-8 flex flex-row  justify-between border-2 border-[#B051EE] w-full min-h-3 rounded-4xl gap-8">
                     <div className="flex flex-col w-1/2 gap-8 justify-between">
                         <div className="flex flex-col gap-8">
                             <div className="flex flex-row gap-8 items-center">
                                 <img className="max-w-24 max-h-24" src={item.icon} />
-                                <h1 className="montserrat text-white font-bold text-5xl">{item.name}</h1>
+                                <div className='flex flex-col'>
+                                    <h1 className="montserrat text-white font-bold text-5xl">{item.name}</h1>
+                                    <h2 className="montserrat text-[#FF5E8A] font-bold text-3xl">{item.year}</h2>
+                                </div>
+                                
                             </div>
                             <p className="montserrat text-white text-2xl">{item.description}</p>
                         </div>
-                        <div className="flex flex-row gap-4">
+                        <div className="flex flex-wrap gap-4">
                             {
                                 item.techs.map((tech) => {
                                     return (
@@ -41,8 +56,32 @@ let ProjectItem = ({item, selectedId, selectProject}) =>
                             }
                         </div>
                     </div>
-                    <div className="flex flex-col justify-center items-center w-1/2">
-                        <img className="max-w-full rounded-3xl" src={item.images[0]} />
+                    <div className="flex flex-col justify-center items-center w-1/2 gap-4">
+                        <img className="max-w-full rounded-3xl h-96" src={item.images[index]} />
+                        <div className="flex flex-row w-full justify-end gap-4">
+                            {
+                                item.githuburl != '' ? (
+                                    <a href={item.githuburl} target="_blank">
+                                        <div className="p-4 border-2 border-[#B051EE] rounded-xl">
+                                            <img className="h-10" src="/img/GithubLogo.png" />
+                                        </div>
+                                    </a>
+                                ) : (
+                                    <></>
+                                )
+                            }
+                            {
+                                item.url != '' ? (
+                                    <a href={item.url} target="_blank">
+                                        <div className="p-4 border-2 border-[#B051EE] rounded-xl">
+                                            <img className="h-10" src="/img/LinkLogo.png" />
+                                        </div>
+                                    </a>
+                                ) : (
+                                    <></>
+                                )
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
--- app/src/components/Projects.jsx
+++ app/src/components/Projects.jsx
@@ -4,41 +4,37 @@ import ProjectItem from './ProjectItem'
 let projectsData = [
     {
         "id" : 0,
-        "name" : "Slydle",
-        "icon" : "/img/LinkedInLogo.png",
-        "description" : "A cool project",
-        "images" : ["/img/test.jpg", "2.png"],
-        "githuburl" : "githublink.com",
-        "url" : "url.com",
-        "techs" : ["tech1", "tech2", "tech3"]
-    },{
+        "name" : "Soly.cool",
+        "icon" : "/img/EtoileLogo2.png",
+        "description" : "This is the site you are actually consulting. Based on a stellar theme, it's designed to display my work and the different projects I have done in my short career.",
+        "images" : ["/img/solycool1.png", "/img/solycool2.png", "/img/solycool3.png"],
+        "githuburl" : "https://github.com/Solymnos/portfolio_2025",
+        "url" : "https://soly.cool",
+        "techs" : ["Vite", "React", "TailwindCSS", "JavaScript", "ThreeJS", "Maya"],
+        "year" : "2025"
+    },
+    {
         "id" : 1,
         "name" : "Slydle",
-        "icon" : "/img/LinkedInLogo.png",
-        "description" : "A cool project",
-        "images" : ["1.png", "2.png"],
-        "githuburl" : "githublink.com",
-        "url" : "url.com",
-        "techs" : ["tech1", "tech2", "tech3"]
-    },{
+        "icon" : "/img/SolaryLogo.png",
+        "description" : "Inspired by concepts like Loldle, Slydle is a game where the player has to guess a personality, based on the esports team Solary. Every day, a new person will be guessable and the player must come back to keep their streak. After more than 250 players, the team decided to make this more official and now hosts the game on their own app.",
+        "images" : ["/img/Slydle1.png", "/img/Slydle2.png", "/img/Slydle3.png"],
+        "githuburl" : "https://github.com/Solymnos/slydle",
+        "url" : "https://slydle.fr",
+        "techs" : ["React", "Styled Components", "JavaScript", "Redux", "Framer", "Axios", "Python", "FastAPI", "MongoDB"],
+        "year" : "2024-2025"
+    },
+    {
         "id" : 2,
-        "name" : "Slydle",
-        "icon" : "/img/LinkedInLogo.png",
-        "description" : "A cool project",
-        "images" : ["1.png", "2.png"],
-        "githuburl" : "githublink.com",
-        "url" : "url.com",
-        "techs" : ["tech1", "tech2", "tech3"]
-    },{
-        "id" : 3,
-        "name" : "Slydle",
-        "icon" : "/img/LinkedInLogo.png",
-        "description" : "A cool project",
-        "images" : ["1.png", "2.png"],
-        "githuburl" : "githublink.com",
-        "url" : "url.com",
-        "techs" : ["tech1", "tech2", "tech3"]
-    }
+        "name" : "Twisted Fate",
+        "icon" : "/img/TFLogo.png",
+        "description" : "In collaboration with a LoL analyst, Twisted Fate is a site that allows people to bet on multiple LoL competitions and win points. The site allows authentication, results, and ranking between participants. The data is based on the Liquipedia API and is fully automated.",
+        "images" : ["/img/TF1.png", "/img/TF2.png"],
+        "githuburl" : "https://github.com/Solymnos/twisted_fate",
+        "url" : "",
+        "techs" : ["Vite", "React", "Styled Components", "JavaScript", "Redux", "Axios", "Python", "FastAPI", "MongoDB", "ShadcnUI"],
+        "year" : "2024-2025"
+    },
 ]
 
 let Projects = () =>
